fc52607b245cf91431b91114745eba590b51b82a
Brent Echols <brentechols34@yahoo.com>
2016-01-07 13:55:20 -0800

    added player controller mock

---
+++ client/Assets/Controls.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets
+{
+    public class ControlConst
+    {
+        private ControlConst(string value) { Value = value; }
+
+        public string Value { get; set; }
+
+        public static ControlConst Ability1 { get { return new ControlConst("Ability1"); } }
+        public static ControlConst Ability2 { get { return new ControlConst("Ability2"); } }
+        public static ControlConst Ability3 { get { return new ControlConst("Ability3"); } }
+        public static ControlConst Ability4 { get { return new ControlConst("Ability4"); } }
+        public static ControlConst Ability5 { get { return new ControlConst("Ability5"); } }
+        public static ControlConst Ability6 { get { return new ControlConst("Ability5"); } }
+        public static ControlConst MoveLeft { get { return new ControlConst("MoveLeft"); } }
+        public static ControlConst MoveRight { get { return new ControlConst("MoveRight"); } }
+        public static ControlConst MoveUp { get { return new ControlConst("MoveUp"); } }
+        public static ControlConst MoveDown { get { return new ControlConst("MoveDown"); } }
+    }
+}
---
+++ client/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var moveVector = new Vector2();
+        if (isPressed(ControlConst.MoveUp))
+        {
+            moveVector = moveVector + new Vector2(0, 1);
+        }
+        if (isPressed(ControlConst.MoveDown))
+        {
+            moveVector += new Vector2(0, -1);
+        }
+        if (isPressed(ControlConst.MoveLeft))
+        {
+            moveVector += new Vector2(-1, 0);
+        }
+        if (isPressed(ControlConst.MoveRight))
+        {
+            moveVector += new Vector2(1, 0);
+        }
+
+        moveVector = moveVector.normalized;
+        Debug.Log(moveVector.ToString());
+	}
+
+    // Returns if the given key axis is pressed
+    bool isPressed(ControlConst cont)
+    {
+        return Input.GetAxis(cont.Value) > 0;
+    }
+}
