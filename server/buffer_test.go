eb3e63535bc00cd9f80437a009bd67ff320872c4
Ben Echols <lologarithm@gmail.com>
2016-01-25 22:24:20 -0700

    Added a buffer benchmark

--- server/buffer_test.go
+++ server/buffer_test.go
@@ -1,8 +1,10 @@
 package server
 
 import (
+	"fmt"
 	"log"
 	"testing"
+	"time"
 )
 
 func TestBytePipe(t *testing.T) {
@@ -29,4 +31,27 @@ func TestBytePipe(t *testing.T) {
 	}
 	n = pipe.Read(buf)
 	log.Printf("Final read: %d", n)
+}
+
+func BenchmarkBytePipe(b *testing.B) {
+	pipe := NewBytePipe(65536)
+	totalBytes := b.N
+	t := time.Now()
+	total := 0
+	buf := make([]byte, 1024)
+	n := (totalBytes * 128) / 1024
+
+	b.ResetTimer()
+	go func() {
+		inbuf := make([]byte, 128)
+		for i := 0; i < totalBytes; i++ {
+			pipe.Write(inbuf)
+		}
+	}()
+
+	for i := 0; i < n; i++ {
+		total += pipe.Read(buf)
+	}
+	b.StopTimer()
+	fmt.Printf("Rate: %.0f bytes/sec\n", float64(total)/time.Now().Sub(t).Seconds())
 }
